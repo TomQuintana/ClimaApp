09b00cccf7948b4fa1a328b8329e4947481e4a37
tomasquintana-gj <tomquintana20@gmail.com>
2023-05-24 21:35:54 -0300

    add first message && search city && listarLugares function

--- app.ts
+++ app.ts
@@ -1,5 +1,7 @@
-import {  inquirerMenu, pausa } from './src/helpers/inquirer'
+import {  inquirerMenu, leerInput, pausa, listarLugares } from './src/helpers/inquirer'
+import Busqueda from './src/helpers/busquedas';
 
+const busquedas = new Busqueda
 let opt:any
 
 console.log('=====================');
@@ -8,6 +10,22 @@ console.log('===================== \n');
 
 while (opt !== 0) {
   opt = await inquirerMenu()
+
+  switch(opt) {
+    case 1 : 
+      // Show message
+      const cityValue = await leerInput()
+
+      // search City
+      const place = await busquedas.ciudad(cityValue)
+      
+      const putPlace = await listarLugares(place);
+      console.log(putPlace);
+      
+      
+
+    break;
+  }
 
   if ( opt !== 0 ) await pausa();
 }
