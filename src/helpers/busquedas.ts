4dcf441ab9bb60e02531f6f29d32719d3fe13db0
tomasquintana-gj <tomquintana20@gmail.com>
2023-05-24 21:36:51 -0300

    add method ciudad for searh a city

--- src/helpers/busquedas.ts
+++ src/helpers/busquedas.ts
@@ -1,19 +1,28 @@
 import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid';
 
 export default class Busqueda {
-
-  historial = ['Barcelona']
-
+  
   constructor() {
   }
 
-  async ciudad(lugar = '') {
-    console.log('Lugar', lugar)
+  async ciudad(lugar: any) {
 
-    // search city
-    const {data} = await axios.get('http://api.openweathermap.org/geo/1.0/direct?q=Barcelona&limit=5&appid=71bd496a0780bcd5c29fd5c670d2c300')
-    console.log(data)
+    try {
 
+      let objCity: Array<any> = []
+      const {data} = await axios.get('http://api.openweathermap.org/geo/1.0/direct?q=Barcelona&limit=5&appid=71bd496a0780bcd5c29fd5c670d2c300')
+      
+      return data.map( (place: any) => ({
+        name: `${ place.name }, ${place.state}, ${place.country}`,
+        id: uuidv4()
+      }))
+  
+    } catch (error) {
+      console.log(error);
+      
+    }
+    
     // TODO: search by lat anf log fot take clima
     
   }
